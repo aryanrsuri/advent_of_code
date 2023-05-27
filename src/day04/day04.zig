aea915cabd5f639dce6d5c61456539cbbf9f1a9a
Aryan Suri <arysuri@proton.me>
2023-05-26 23:48:08 -0700

    4 done

--- src/day04/day04.zig
+++ src/day04/day04.zig
@@ -3,27 +3,59 @@ var gpa_init = std.heap.GeneralPurposeAllocator(.{}){};
 const gpa = gpa_init.allocator();
 const file = @embedFile("./input");
 const print = std.debug.print;
+const assigment = struct {
+    low: u8,
+    high: u8,
 
-fn part01() !u8 {
-    var buffer = std.mem.tokenize(u8, file, "\r\n");
-    while (buffer.next()) |line| {
-        var buffer_comma = std.mem.tokenize(u8, line, ",");
-        while (buffer_comma.next()) |elf| {
-            // for (line) |char| {
-            var nums = try split_n(elf, "-", 2);
-            // var res = try std.fmt.parseInt(u8, nums[0], 10);
-            // var res = try std.fmt.parseInt(u8, nums[1], 10);
-            print("{s}\n", .{nums});
-        }
-        // }
-        // var assignments = std.mem.split(u8, line, ",");
-        // var asn1 = assignments.first();
-        // var asn2 = assignments.next().?;
-        // print("{s}\n", .{asn1});
-        // print("{s}\n", .{asn2});
+    fn parse(input: []const u8) !assigment {
+        var nums = try split_n(input, "-", 2);
+        return assigment{
+            .low = try std.fmt.parseUnsigned(u8, nums[0], 10),
+            .high = try std.fmt.parseUnsigned(u8, nums[1], 10),
+        };
     }
 
-    return 0;
+    fn contain(self: assigment, other: assigment) bool {
+        return other.low >= self.low and other.high <= self.high;
+    }
+
+    fn overlap(self: assigment, other: assigment) bool {
+        return (self.low <= other.low and self.high >= other.low) or (other.low <= self.low and other.high >= self.high);
+    }
+};
+fn part01() !u64 {
+    var buffer = std.mem.tokenize(u8, file, "\r\n");
+    var count: u64 = 0;
+    while (buffer.next()) |line| {
+        var buffer_comma = try split_n(line, ",", 2);
+        var asn1 = try assigment.parse(buffer_comma[0]);
+        var asn2 = try assigment.parse(buffer_comma[1]);
+
+        if (asn1.contain(asn2) or asn2.contain(asn1)) {
+            count += 1;
+        } else {
+            continue;
+        }
+    }
+
+    return count;
+}
+fn part02() !u64 {
+    var buffer = std.mem.tokenize(u8, file, "\r\n");
+    var count: u64 = 0;
+    while (buffer.next()) |line| {
+        var buffer_comma = try split_n(line, ",", 2);
+        var asn1 = try assigment.parse(buffer_comma[0]);
+        var asn2 = try assigment.parse(buffer_comma[1]);
+
+        if (asn1.overlap(asn2) or asn2.overlap(asn1)) {
+            count += 1;
+        } else {
+            continue;
+        }
+    }
+
+    return count;
 }
 pub fn is_number(char: u8) bool {
     if ('0' <= char and char <= '9') {
@@ -35,7 +67,9 @@ pub fn is_number(char: u8) bool {
 
 test "part1" {
     const res = try part01();
+    const res1 = try part02();
     print("{}\r\n", .{res});
+    print("{}\r\n", .{res1});
 }
 
 pub fn split_n(input: []const u8, delim: []const u8, comptime n: usize) ![n][]const u8 {
